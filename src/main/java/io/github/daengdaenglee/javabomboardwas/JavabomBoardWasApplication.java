e89d2d62929425029af2e71e3de799ebc4415e31
daengdaengLee <gunho1020@gmail.com>
2019-05-30 12:33:36 +0900

    ENH: Init Project

---
+++ src/main/java/io/github/daengdaenglee/javabomboardwas/JavabomBoardWasApplication.java
@@ -0,0 +1,13 @@
+package io.github.daengdaenglee.javabomboardwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabomBoardWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabomBoardWasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/daengdaenglee/javabomboardwas/JavabomBoardWasApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.daengdaenglee.javabomboardwas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavabomBoardWasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
