c27ed9e7548e07c3498743546cd2428e37658262
Kunho Lee <gunho1020@gmail.com>
2019-06-01 21:38:09 +0900

    ENH #5(Finish): Article 서비스 클래스 초기화 (#8)

---
+++ src/main/java/io/github/daengdaenglee/javabomboardwas/services/ArticleService.java
@@ -0,0 +1,30 @@
+package io.github.daengdaenglee.javabomboardwas.services;
+
+import io.github.daengdaenglee.javabomboardwas.entities.Article;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ArticleService {
+    public List<Article> getAllArticles() {
+        return new ArrayList<Article>();
+    }
+
+    public Article getArticleById(String id) {
+        return new Article(id, null, null);
+    }
+
+    public Article makeNewArticle(Article article) {
+        return article;
+    }
+
+    public Article changeArticle(Article article) {
+        return article;
+    }
+
+    public Boolean deleteArticleById(String id) {
+        return true;
+    }
+}
