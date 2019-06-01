c5ed5b45282f659cfeff5f2be5f0eb66ebc827a8
Kunho Lee <gunho1020@gmail.com>
2019-06-01 22:49:50 +0900

    ENH #11(Finish): Article 엔티티 객체를 JSON 문자열로 변환하는 클래스 구현 (#12)

--- src/main/java/io/github/daengdaenglee/javabomboardwas/controllers/ArticleController.java
+++ src/main/java/io/github/daengdaenglee/javabomboardwas/controllers/ArticleController.java
@@ -1,8 +1,12 @@
 package io.github.daengdaenglee.javabomboardwas.controllers;
 
+import io.github.daengdaenglee.javabomboardwas.entities.Article;
 import io.github.daengdaenglee.javabomboardwas.services.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -33,5 +37,19 @@ public class ArticleController {
     @DeleteMapping("/articles/{articleId}")
     public String deleteArticle(@PathVariable String articleId) {
         return "/articles/" + articleId;
+    }
+
+    public static class ArticleJSON {
+        public String type = "articles";
+        public String id;
+        public Map<String, String> attributes = new HashMap<>();
+        public Map<String, String> links = new HashMap<>();
+
+        public ArticleJSON(Article article) {
+            this.id = article.id;
+            this.attributes.put("title", article.title);
+            this.attributes.put("body", article.body);
+            this.links.put("self", "/api/v1/articles/" + article.id);
+        }
     }
 }
