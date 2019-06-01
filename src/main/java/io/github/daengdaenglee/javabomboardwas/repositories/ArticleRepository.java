c721d39bdba5026f25e2d0cbd676259aef17bd2e
daengdaengLee <gunho1020@gmail.com>
2019-06-02 00:26:30 +0900

    각 메소드 초기화

--- src/main/java/io/github/daengdaenglee/javabomboardwas/repositories/ArticleRepository.java
+++ src/main/java/io/github/daengdaenglee/javabomboardwas/repositories/ArticleRepository.java
@@ -1,7 +1,29 @@
 package io.github.daengdaenglee.javabomboardwas.repositories;
 
+import io.github.daengdaenglee.javabomboardwas.entities.Article;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class ArticleRepository {
+    public Article insert(Article article) {
+        return article;
+    }
+
+    public Article selectById(String id) {
+        return new Article(id, null,null);
+    }
+
+    public List<Article> selectAll() {
+        return new ArrayList<>();
+    }
+
+    public Article update(Article article) {
+        return article;
+    }
+
+    public void delete(String id) {
+    }
 }
