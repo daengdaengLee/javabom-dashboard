bbe3fdded588f37c0c69b7fbf0d094161facee4c
daengdaengLee <gunho1020@gmail.com>
2019-06-01 21:05:21 +0900

    Article 엔티티 클래스의 속성과 생성자 구현

--- src/main/java/io/github/daengdaenglee/javabomboardwas/entities/Article.java
+++ src/main/java/io/github/daengdaenglee/javabomboardwas/entities/Article.java
@@ -1,4 +1,13 @@
 package io.github.daengdaenglee.javabomboardwas.entities;
 
 public class Article {
+    public String id;
+    public String title;
+    public String body;
+
+    public Article(String id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
 }
