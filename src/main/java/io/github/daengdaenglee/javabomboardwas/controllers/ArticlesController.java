b44b6fcf14d03b5620f597ca96421b96a63695c3
daengdaengLee <gunho1020@gmail.com>
2019-06-01 19:29:47 +0900

    listAllArticles API 초기화

--- src/main/java/io/github/daengdaenglee/javabomboardwas/controllers/ArticlesController.java
+++ src/main/java/io/github/daengdaenglee/javabomboardwas/controllers/ArticlesController.java
@@ -1,9 +1,14 @@
 package io.github.daengdaenglee.javabomboardwas.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ArticlesController {
+    @GetMapping("/articles")
+    public String listAllArticles() {
+        return "/articles";
+    }
 }
