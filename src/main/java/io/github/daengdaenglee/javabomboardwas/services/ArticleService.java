f465311b3e6c11c69c2d89a03a5711f6accda150
daengdaengLee <gunho1020@gmail.com>
2019-06-01 21:31:11 +0900

    getAllArticles 메소드 초기화

--- src/main/java/io/github/daengdaenglee/javabomboardwas/services/ArticleService.java
+++ src/main/java/io/github/daengdaenglee/javabomboardwas/services/ArticleService.java
@@ -1,7 +1,14 @@
 package io.github.daengdaenglee.javabomboardwas.services;
 
+import io.github.daengdaenglee.javabomboardwas.entities.Article;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ArticleService {
+    public List<Article> getAllArticles() {
+        return new ArrayList<Article>();
+    }
 }
