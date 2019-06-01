b2f9b00be336f247eae102aebbc673f57905ef8f
daengdaengLee <gunho1020@gmail.com>
2019-06-01 21:36:23 +0900

    deleteArticleById 메소드 초기화

--- src/main/java/io/github/daengdaenglee/javabomboardwas/services/ArticleService.java
+++ src/main/java/io/github/daengdaenglee/javabomboardwas/services/ArticleService.java
@@ -23,4 +23,8 @@ public class ArticleService {
     public Article changeArticle(Article article) {
         return article;
     }
+
+    public Boolean deleteArticleById(String id) {
+        return true;
+    }
 }
