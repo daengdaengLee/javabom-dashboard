2d6b8b2d798a782df274060426de512452537e15
daengdaengLee <gunho1020@gmail.com>
2019-06-02 09:30:44 +0900

    ArticleService의 makeNewArticle 메소드 구현

--- src/main/java/io/github/daengdaenglee/javabomboardwas/services/ArticleService.java
+++ src/main/java/io/github/daengdaenglee/javabomboardwas/services/ArticleService.java
@@ -1,13 +1,19 @@
 package io.github.daengdaenglee.javabomboardwas.services;
 
 import io.github.daengdaenglee.javabomboardwas.entities.Article;
+import io.github.daengdaenglee.javabomboardwas.repositories.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ArticleService {
+    @Autowired
+    public ArticleRepository articleRepository;
+
     public List<Article> getAllArticles() {
         return new ArrayList<Article>();
     }
@@ -16,8 +22,11 @@ public class ArticleService {
         return new Article(id, null, null);
     }
 
-    public Article makeNewArticle(String title, String body) {
+    public Article makeNewArticle(String title, String body) throws IOException {
         Article article = new Article(null, title, body);
+
+        article = articleRepository.insert(article);
+
         return article;
     }
 
