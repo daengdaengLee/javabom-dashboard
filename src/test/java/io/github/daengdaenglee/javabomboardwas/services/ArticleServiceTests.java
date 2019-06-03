4d75ee6caa43b2bb4a373d7c8f6099ae4b94a525
daengdaengLee <gunho1020@gmail.com>
2019-06-03 13:03:02 +0900

    ArticleService constructor 테스트

--- src/test/java/io/github/daengdaenglee/javabomboardwas/services/ArticleServiceTests.java
+++ src/test/java/io/github/daengdaenglee/javabomboardwas/services/ArticleServiceTests.java
@@ -1,4 +1,13 @@
 package io.github.daengdaenglee.javabomboardwas.services;
 
+import io.github.daengdaenglee.javabomboardwas.repositories.ArticleRepository;
+import org.junit.Test;
+
 public class ArticleServiceTests {
+    private static final ArticleRepository articleRepository = new ArticleRepository();
+
+    @Test
+    public void constructorWithArticleRepository() {
+        new ArticleService(articleRepository);
+    }
 }
