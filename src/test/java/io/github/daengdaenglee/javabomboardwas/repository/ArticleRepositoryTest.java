215b0ae84846694b44fdd669a57b012b9fa66870
daengdaengLee <gunho1020@gmail.com>
2019-06-17 20:39:42 +0900

    TST: ArticleRespository save 메소드 테스트
    정상적인 Article 객체가 주어졌을 때
    id, links 와 같은 정보가 제대로 생성되었는지 확인

---
+++ src/test/java/io/github/daengdaenglee/javabomboardwas/repository/ArticleRepositoryTest.java
@@ -0,0 +1,38 @@
+package io.github.daengdaenglee.javabomboardwas.repository;
+
+import io.github.daengdaenglee.javabomboardwas.entity.article.article.Article;
+import io.github.daengdaenglee.javabomboardwas.entity.article.article.Attribute;
+import io.github.daengdaenglee.javabomboardwas.entity.article.article.Link;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArticleRepositoryTest {
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @Test
+    public void saveArticle_withArticle_success_returnArticle() {
+        // given
+        Article input = Article.builder()
+                .attributes(Attribute.builder()
+                        .title("Test Article")
+                        .content("This is test article")
+                        .build())
+                .build();
+
+        // when
+        Article output = articleRepository.save(input);
+
+        // then
+        assertThat(output.getAttributes()).isEqualTo(input.getAttributes());
+        assertThat(output.getId()).isEqualTo(output.getPk().toString());
+        assertThat(output.getLinks().getSelf()).isEqualTo("/articles/" + output.getId());
+    }
+}
