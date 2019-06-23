7d33ac9a360ec80d9a5a119803e9ea22b90c57a7
daengdaengLee <gunho1020@gmail.com>
2019-06-23 14:21:39 +0900

    ENH: Article 모델, 엔티티 간 변환 코덱 생성

---
+++ src/main/java/io/github/daengdaenglee/javabomboardwas/codec/article/ArticleCodec.java
@@ -0,0 +1,45 @@
+package io.github.daengdaenglee.javabomboardwas.codec.article;
+
+import io.github.daengdaenglee.javabomboardwas.entity.article.Article;
+import io.github.daengdaenglee.javabomboardwas.entity.article.Attribute;
+import io.github.daengdaenglee.javabomboardwas.model.article.ArticleJSON;
+import io.github.daengdaenglee.javabomboardwas.model.article.AttributesJSON;
+import io.github.daengdaenglee.javabomboardwas.model.article.LinksJSON;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArticleCodec {
+    public ArticleJSON fromEntityToModel(final Article article) {
+        final String articleId = article.getId().toString();
+
+        final String title = article.getAttributes().getTitle();
+        final String content = article.getAttributes().getContent();
+        final AttributesJSON attributes = AttributesJSON.builder()
+                .title(title)
+                .content(content)
+                .build();
+
+        return ArticleJSON.builder()
+                .id(articleId)
+                .type("articles")
+                .links(new LinksJSON("/api/v1/articles/" + articleId))
+                .attributes(attributes)
+                .build();
+    }
+
+    public Article fromModelToEntity(final ArticleJSON articleJSON) {
+        final String id = articleJSON.getId();
+
+        final String title = articleJSON.getAttributes().getTitle();
+        final String content = articleJSON.getAttributes().getContent();
+        final Attribute attributes = Attribute.builder()
+                .title(title)
+                .content(content)
+                .build();
+
+        return Article.builder()
+                .id(id == null ? null : Long.valueOf(id))
+                .attributes(attributes)
+                .build();
+    }
+}
