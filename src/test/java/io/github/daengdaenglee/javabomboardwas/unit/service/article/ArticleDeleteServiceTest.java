722cf3ec42c11896be75fbd4dde1e06c0ee37837
daengdaengLee <gunho1020@gmail.com>
2019-06-23 22:36:20 +0900

    TST: ArticleDeleteTest 유닛테스트 작성

---
+++ src/test/java/io/github/daengdaenglee/javabomboardwas/unit/service/article/ArticleDeleteServiceTest.java
@@ -0,0 +1,86 @@
+package io.github.daengdaenglee.javabomboardwas.unit.service.article;
+
+import io.github.daengdaenglee.javabomboardwas.entity.article.Article;
+import io.github.daengdaenglee.javabomboardwas.entity.article.Attribute;
+import io.github.daengdaenglee.javabomboardwas.repository.ArticleRepository;
+import io.github.daengdaenglee.javabomboardwas.service.article.ArticleDeleteService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+public class ArticleDeleteServiceTest {
+    @MockBean
+    private ArticleRepository articleRepository;
+
+    private ArticleDeleteService articleDeleteService;
+
+    private String givenIdString1;
+    private Long givenIdLong1;
+    private Article givenArticle1;
+
+    @Before
+    public void setUp() {
+        givenIdString1 = "1";
+        givenIdLong1 = 1L;
+        givenArticle1 = Article.builder()
+                .id(givenIdLong1)
+                .attributes(Attribute.builder()
+                        .title("Test")
+                        .content("Test article")
+                        .build())
+                .build();
+
+        articleDeleteService = new ArticleDeleteService(articleRepository);
+    }
+
+    @Test
+    public void deleteById_withStringIdOfExistArticle_SuccessWithMonoEmpty() {
+        // given
+        when(articleRepository.findById(givenIdLong1)).thenReturn(Optional.of(givenArticle1));
+
+        // when
+        Mono<Void> received = articleDeleteService.deleteById(givenIdString1);
+
+        // then
+        StepVerifier.create(received)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void deleteById_withStringIdOfNotExistArticle_SuccessWithMonoEmpty() {
+        // given
+        when(articleRepository.findById(givenIdLong1)).thenReturn(Optional.empty());
+
+        // when
+        Mono<Void> received = articleDeleteService.deleteById(givenIdString1);
+
+        // then
+        StepVerifier.create(received)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void deleteById_withStringIdCantBoLong_FailWithNumberFormatException() {
+        // given
+        String wrongId = "ABC";
+
+        // when
+        Mono<Void> received = articleDeleteService.deleteById(wrongId);
+
+        // then
+        StepVerifier.create(received)
+                .expectError(NumberFormatException.class)
+                .verify();
+    }
+}
