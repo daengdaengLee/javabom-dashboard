eb7746b2e2b9091a11127b5914d134e0bff0ff0a
daengdaengLee <gunho1020@gmail.com>
2019-06-24 19:38:11 +0900

    TST: CommonConstant 유닛 테스트 작성

---
+++ src/test/java/io/github/daengdaenglee/javabomboardwas/unit/constant/CommonConstantTest.java
@@ -0,0 +1,14 @@
+package io.github.daengdaenglee.javabomboardwas.unit.constant;
+
+import io.github.daengdaenglee.javabomboardwas.constant.CommonConstant;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class CommonConstantTest {
+    @Test
+    public void instantiate() {
+        new CommonConstant();
+    }
+}
