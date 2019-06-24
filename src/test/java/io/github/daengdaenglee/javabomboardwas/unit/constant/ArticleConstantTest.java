8e8e818e483373f00d5c5f2e9cc719eaca138582
daengdaengLee <gunho1020@gmail.com>
2019-06-24 19:36:59 +0900

    TST: ArticleConstant 유닛 테스트 작성

---
+++ src/test/java/io/github/daengdaenglee/javabomboardwas/unit/constant/ArticleConstantTest.java
@@ -0,0 +1,14 @@
+package io.github.daengdaenglee.javabomboardwas.unit.constant;
+
+import io.github.daengdaenglee.javabomboardwas.constant.ArticleConstant;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class ArticleConstantTest {
+    @Test
+    public void instantiate() {
+        new ArticleConstant();
+    }
+}
