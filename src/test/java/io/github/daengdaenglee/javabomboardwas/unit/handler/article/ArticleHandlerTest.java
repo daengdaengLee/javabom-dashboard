19d5557a98bcd1a3c84cd1892d36b0ec51aefcfd
daengdaengLee <gunho1020@gmail.com>
2019-06-27 23:31:45 +0900

    TST: ArticleHandler 유닛 테스트 작성

---
+++ src/test/java/io/github/daengdaenglee/javabomboardwas/unit/handler/article/ArticleHandlerTest.java
@@ -0,0 +1,274 @@
+package io.github.daengdaenglee.javabomboardwas.unit.handler.article;
+
+import io.github.daengdaenglee.javabomboardwas.constant.ArticleConstant;
+import io.github.daengdaenglee.javabomboardwas.constant.CommonConstant;
+import io.github.daengdaenglee.javabomboardwas.handler.article.ArticleHandler;
+import io.github.daengdaenglee.javabomboardwas.model.article.ArticleJSON;
+import io.github.daengdaenglee.javabomboardwas.model.article.AttributesJSON;
+import io.github.daengdaenglee.javabomboardwas.model.article.LinksJSON;
+import io.github.daengdaenglee.javabomboardwas.model.container.DataContainer;
+import io.github.daengdaenglee.javabomboardwas.service.article.ArticleDeleteService;
+import io.github.daengdaenglee.javabomboardwas.service.article.ArticleFindService;
+import io.github.daengdaenglee.javabomboardwas.service.article.ArticleSaveService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+public class ArticleHandlerTest {
+    private ArticleHandler articleHandler;
+    private WebTestClient client;
+
+    @MockBean
+    private ArticleSaveService articleSaveService;
+    @MockBean
+    private ArticleFindService articleFindService;
+    @MockBean
+    private ArticleDeleteService articleDeleteService;
+
+    private ArticleJSON articleJSON1;
+    private ArticleJSON articleJSON2;
+
+    @Before
+    public void setUp() {
+        articleJSON1 = ArticleJSON.builder()
+                .id("1")
+                .type(ArticleConstant.TYPE_ARTICLES)
+                .links(new LinksJSON(CommonConstant.BASE_PATH + ArticleConstant.BASE_PATH + "/1"))
+                .attributes(AttributesJSON.builder()
+                        .title("Article 1")
+                        .content("article 1")
+                        .build())
+                .build();
+
+        articleJSON2 = ArticleJSON.builder()
+                .id("2")
+                .type(ArticleConstant.TYPE_ARTICLES)
+                .links(new LinksJSON(CommonConstant.BASE_PATH + ArticleConstant.BASE_PATH + "/2"))
+                .attributes(AttributesJSON.builder()
+                        .title("Article 2")
+                        .content("article 2")
+                        .build())
+                .build();
+
+        articleHandler = new ArticleHandler(
+                articleFindService,
+                articleSaveService,
+                articleDeleteService
+        );
+    }
+
+    @Test
+    public void listAllArticles_whenArticlesExist_successReturnServerResponseContainingArticlesList() {
+        // given
+        String pattern = ArticleConstant.BASE_PATH;
+        String uri = ArticleConstant.BASE_PATH;
+        RouterFunction<ServerResponse> routerFunction = RouterFunctions
+                .route()
+                .GET(pattern, articleHandler::listAllArticles)
+                .build();
+        client = WebTestClient
+                .bindToRouterFunction(routerFunction)
+                .build();
+
+        when(articleFindService.findAll()).thenReturn(Flux.just(articleJSON1, articleJSON2));
+
+        // when
+        WebTestClient.ResponseSpec received = client.get().uri(uri).exchange();
+
+        // then
+        received
+                .expectStatus()
+                .isOk()
+                .expectBody(new ParameterizedTypeReference<DataContainer<List<ArticleJSON>>>() {
+                })
+                .value(dataContainer -> {
+                    List<ArticleJSON> receivedArticles = dataContainer.getData();
+
+                    ArticleJSON receivedArticle1 = receivedArticles.get(0);
+                    ArticleJSON receivedArticle2 = receivedArticles.get(1);
+
+                    assertArticleJSON(receivedArticle1, articleJSON1);
+                    assertArticleJSON(receivedArticle2, articleJSON2);
+                });
+    }
+
+    @Test
+    public void readArticle_whenArticleExist_successReturnServerResponseContainingArticle() {
+        // given
+        String pattern = ArticleConstant.BASE_PATH + "/{" + ArticleConstant.PATH_VAR_ARTICLE_ID + "}";
+        String uri = ArticleConstant.BASE_PATH + "/" + articleJSON1.getId();
+        RouterFunction<ServerResponse> routerFunction = RouterFunctions
+                .route()
+                .GET(pattern, articleHandler::readArticle)
+                .build();
+        client = WebTestClient
+                .bindToRouterFunction(routerFunction)
+                .build();
+
+        when(articleFindService.findById(articleJSON1.getId())).thenReturn(Mono.just(articleJSON1));
+
+        // when
+        WebTestClient.ResponseSpec received = client.get().uri(uri).exchange();
+
+        // then
+        received
+                .expectStatus()
+                .isOk()
+                .expectBody(new ParameterizedTypeReference<DataContainer<ArticleJSON>>() {
+                })
+                .value(dataContainer -> {
+                    ArticleJSON receivedArticle = dataContainer.getData();
+
+                    assertArticleJSON(receivedArticle, articleJSON1);
+                });
+    }
+
+    @Test
+    public void createArticle_withArticleJSONToCreate_successReturnCreatedArticleJSON() {
+        // given
+        String pattern = ArticleConstant.BASE_PATH;
+        String uri = ArticleConstant.BASE_PATH;
+        RouterFunction<ServerResponse> routerFunction = RouterFunctions
+                .route()
+                .POST(pattern, articleHandler::createArticle)
+                .build();
+        client = WebTestClient
+                .bindToRouterFunction(routerFunction)
+                .build();
+
+        ArticleJSON inputArticle = ArticleJSON.builder()
+                .attributes(AttributesJSON.builder()
+                        .title(articleJSON1.getAttributes().getTitle())
+                        .content(articleJSON1.getAttributes().getContent())
+                        .build())
+                .build();
+        when(articleSaveService.create(any(ArticleJSON.class))).thenReturn(Mono.just(articleJSON1));
+
+        // when
+        WebTestClient.ResponseSpec received = client
+                .post()
+                .uri(uri)
+                .syncBody(new DataContainer<>(inputArticle))
+                .exchange();
+
+        // then
+        received
+                .expectStatus()
+                .isCreated()
+                .expectBody(new ParameterizedTypeReference<DataContainer<ArticleJSON>>() {
+                })
+                .value(dataContainer -> {
+                    ArticleJSON receivedArticle = dataContainer.getData();
+
+                    assertArticleJSON(receivedArticle, articleJSON1);
+                });
+    }
+
+    @Test
+    public void updateArticle_withExistArticleAndBothChangedTitleAndContent_successReturnChangedArticleJSON() {
+        // given
+        String articleId = articleJSON1.getId();
+        String pattern = ArticleConstant.BASE_PATH + "/{" + ArticleConstant.PATH_VAR_ARTICLE_ID + "}";
+        String uri = ArticleConstant.BASE_PATH + "/" + articleId;
+        RouterFunction<ServerResponse> routerFunction = RouterFunctions
+                .route()
+                .PUT(pattern, articleHandler::updateArticle)
+                .build();
+        client = WebTestClient
+                .bindToRouterFunction(routerFunction)
+                .build();
+
+        String changedTitle = articleJSON1.getAttributes().getTitle() + " changed";
+        String changedContent = articleJSON1.getAttributes().getContent() + " changed";
+        ArticleJSON inputArticle = ArticleJSON.builder()
+                .attributes(AttributesJSON.builder()
+                        .title(changedTitle)
+                        .content(changedContent)
+                        .build())
+                .build();
+        ArticleJSON changedArticle = ArticleJSON.builder()
+                .id(articleId)
+                .attributes(AttributesJSON.builder()
+                        .title(changedTitle)
+                        .content(changedContent)
+                        .build())
+                .type(ArticleConstant.TYPE_ARTICLES)
+                .links(new LinksJSON(CommonConstant.BASE_PATH + ArticleConstant.BASE_PATH + "/" + articleId))
+                .build();
+        when(articleSaveService.update(eq(articleId), any(ArticleJSON.class))).thenReturn(Mono.just(changedArticle));
+
+        // when
+        WebTestClient.ResponseSpec received = client
+                .put()
+                .uri(uri)
+                .syncBody(new DataContainer<>(inputArticle))
+                .exchange();
+
+        // then
+        received
+                .expectStatus()
+                .isOk()
+                .expectBody(new ParameterizedTypeReference<DataContainer<ArticleJSON>>() {
+                })
+                .value(dataContainer -> {
+                    ArticleJSON receivedArticle = dataContainer.getData();
+
+                    assertArticleJSON(receivedArticle, changedArticle);
+                });
+    }
+
+    @Test
+    public void deleteArticle_withExistArticleId_successReturnOk() {
+        // given
+        String articleId = articleJSON1.getId();
+        String pattern = ArticleConstant.BASE_PATH + "/{" + ArticleConstant.PATH_VAR_ARTICLE_ID + "}";
+        String uri = ArticleConstant.BASE_PATH + "/" + articleId;
+        RouterFunction<ServerResponse> routerFunction = RouterFunctions
+                .route()
+                .DELETE(pattern, articleHandler::deleteArticle)
+                .build();
+        client = WebTestClient
+                .bindToRouterFunction(routerFunction)
+                .build();
+
+        when(articleDeleteService.deleteById(articleId)).thenReturn(Mono.empty());
+
+        // when
+        WebTestClient.ResponseSpec received = client
+                .delete()
+                .uri(uri)
+                .exchange();
+
+        // then
+        received
+                .expectStatus()
+                .isOk();
+    }
+
+    private void assertArticleJSON(final ArticleJSON received, final ArticleJSON expected) throws AssertionError {
+        assertThat(received.getId()).isEqualTo(expected.getId());
+        assertThat(received.getAttributes().getTitle())
+                .isEqualTo(expected.getAttributes().getTitle());
+        assertThat(received.getAttributes().getContent())
+                .isEqualTo(expected.getAttributes().getContent());
+        assertThat(received.getType()).isEqualTo(expected.getType());
+        assertThat(received.getLinks().getSelf()).isEqualTo(expected.getLinks().getSelf());
+    }
+}
